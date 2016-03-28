9746b7e8fb6929847181fb47971ad876bf045d99
wook971215 <skrth00@gmail.com>
2016-03-28 13:47:44 +0900

    week4 201611107정현욱

--- 201611107.py
+++ 201611107.py
@@ -1,6 +1,14 @@
-﻿def makeSwirlSquare(size,bigger,sides,angle):
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def makeSwirlSquare(size,bigger,sides,angle):
      for i in range(0,sides):
           if(i%2):
               size=size+bigger
           t1.fd(size)
           t1.right(90)
+
+makeSwirlSquare(10,10,20,90)
+
+wn.exitonclick()
