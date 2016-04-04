8f96fb25c1a64074193e5aa82907be4e4b5542f0
pce913 <pce0913@naver.com>
2016-04-04 13:19:05 +0900

    w6main.py
    201611107 정현욱

---
+++ w6main.py
@@ -0,0 +1,13 @@
+﻿import turtle
+wn=turtle.Screen()
+
+print "Draw star triangle!"
+print "input size"
+
+nMax=raw_input("size:")
+nMax=int(nMax)
+
+for i in range(nMax):
+    print " "*(nMax-i)+"*"*(i*2-1)
+
+wn.exitonclick()
