c4e7e43eee6a994199a4951e887ea88343976286
joowonsuk <ty921115@naver.com>
2016-04-06 15:42:41 +0900

    w6main(9)
    201611107 정현욱

---
+++ w6main(9).py
@@ -0,0 +1,23 @@
+﻿import turtle
+wn=turtle.Screen()
+
+print "Sum of Multiples of 3 or 5!"
+
+begin=raw_input("input begin number:")
+
+end=raw_input("input end number:")
+
+begin=int(begin)
+end=int(end)
+
+def sumOfMultiplesOf3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+        if i%3==0 or i%5==0:
+            sum=sum+i
+    print sum
+    return sum
+
+sumOfMultiplesOf3_5(1,1000)
+
+wn.exitonclick()
