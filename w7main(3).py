ad96ec221ce4b9257eb45a6aebf8bcfa32b71b1a
Hyeon Wook Jeong (정현욱) 201611107 <wook971215@naver.com>
2016-04-18 22:56:40 +0900

    w7main(3)
    201611107정현욱

---
+++ w7main(3).py
@@ -0,0 +1,28 @@
+﻿"""
+@author JHU
+@since 160418
+"""
+
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+t1.home()
+t1.clear()
+
+def drawSquareFrom():
+	tracks=([100,0],[100,100],[0,100],[0,0])
+	t1.pendown()
+	for i in range(0,4):
+		t1.goto(tracks[i])
+        
+def lab7():
+    drawSquareFrom()
+    wn.exitonclick()
+
+def main():
+    lab7()
+
+if __name__=="__main__":
+    main()
