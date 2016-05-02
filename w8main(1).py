a2da637bb556bac872785dcf30e8b4f735a35edf
Suman Hong <suman0204@naver,com>
2016-05-02 13:12:15 +0900

    w8main(1).py
    201611107 정현욱

---
+++ w8main(1).py
@@ -0,0 +1,21 @@
+﻿def charCount():
+	d=dict()
+	word=raw_input('input word :')
+    
+	for c in word:
+		if c not in d:
+			d[c]=1
+		else:
+			d[c]=d[c]+1
+    
+	print d
+
+def lab8():
+	charCount()
+
+def main():
+	lab8()
+
+if __name__=="__main__":
+	main()
+	raw_input()
