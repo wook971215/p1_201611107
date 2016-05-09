dea6e08cc19856ac4e8c6522dd49267d733c3cec
daseul21 <daseul_kim@icloud.com>
2016-05-09 13:36:13 +0900

    10주차 Milk 비율 구하기

---
+++ w10main(1).py
@@ -0,0 +1,15 @@
+﻿Rate=[["Coffee","Water","Milk","Icecream"],
+  ["Espresso","No","No","No"],
+  ["Long Black","Yes","No","No"],
+ ["Flat White","No","Yes","No"],
+ ["Cappuccino","No","Yes - Frothy","No"],
+ ["Affogato","No","No","Yes"]]
+
+data= Rate[1:]
+datav=list()
+
+for i in data:
+    if i[2]=="Yes" or i[2]=="Yes - Frothy":
+        datav.append(i[0])
+        
+float(len(data))/float(len(datav))
