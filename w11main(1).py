9ae96df8e4674a511e2ac2570c161130f9a358c0
Hyeon Wook Jeong (정현욱) 201611107 <wook971215@naver.com>
2016-05-13 16:59:13 +0900

    수업시간 좌표

---
+++ w11main(1).py
@@ -0,0 +1,39 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.penup()
+t2=turtle.Turtle()
+t2.penup()
+t2.goto(-100,100)
+t2.pendown()
+t2.fd(200)
+
+def keyup():
+    t1.fd(50)
+def keydown():
+    t1.back(50)
+def turnr():
+    t1.right(90)
+def turnl():
+    t1.left(90)
+
+def mousegoto(a,b):
+    t1.setpos(a,b)
+    (a,b)=t1.pos() 
+    if -80<a<80 and 95<b<105:
+        print "Correct"
+
+def addkeys():
+    wn.onkey(keyup, "Up")
+    wn.onkey(keydown, "Down")
+    wn.onkey(turnr, "Right")
+    wn.onkey(turnl, "Left")
+    
+def addmouse():
+    wn.onclick(mousegoto)
+ 
+addkeys()
+addmouse()
+
+wn.listen()
+turtle.mainloop()
